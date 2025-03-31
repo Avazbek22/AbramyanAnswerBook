eb039b1cdf2b862bdd41379ef8a11548499deaa2
Avazbek Olimov <avazbekolimov722@gmail.com>
2025-03-31 22:15:19 +0300

    Added While

--- AbramyanAnswerBook/For.cs
+++ AbramyanAnswerBook/For.cs
@@ -268,18 +268,94 @@
 // }
 // Console.WriteLine($"Sum = {sum}");
     
+// For 33. Дано целое число N (> 1). Последовательность чисел Фибоначчи FK (целого типа) определяется следующим образом:
+// F1 = 1, F2 = 1, FK = FK−2 + FK−1, K = 3, 4, . . . .
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int f1 = 0, f2 = 1, fk = 0;
+// Console.Write(f1 + " ");
+// for (int i = 1; i < n; i++)
+// {
+//     fk = f1 + f2;
+//     f1 = f2;
+//     f2 = fk;
+//     Console.Write(fk + " ");
+// }
 
+// For 36. Даны целые положительные числа N и K. Найти сумму 1^K + 2^K + . . . + N^K.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter K: ");
+// int k = int.Parse(Console.ReadLine()!);
+// int result = 0;
+// for (int i = 1; i <= n; i++)
+// {
+//     int power = 1;
+//     for (int j = 0; j < k; j++)
+//     {
+//         power *= i;
+//     }
+//     result += power;
+//     Console.WriteLine(result);
+// }
 
+// For37. Дано целое число N (> 0). Найти сумму1^1 + 2^2 + . . . + N^N .
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int sum = 0;
+// for (int i = 1; i <= n; i++)
+// {
+//     int power = 1;
+//     for (int j = 1; j <= i; j++)
+//     {
+//         power *= i;
+//     }
+//
+//     sum += power;
+//     Console.Write(sum + " ");
+// }
 
+// For38. Дано целое число N (> 0). Найти сумму 1^N + 2^N−1 + . . . + N^1
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int sum = 0;
+// for (int i = 1; i <= n; i++)
+// {
+//     int power = 1;
+//     for (int j = 0; j <= n - i; j++)
+//     {
+//         power *= i;
+//     }
+//
+//     sum += power;
+//     Console.Write(sum + " ");
+// }
 
+// For39. Даны целые положительные числа A и B (A < B).
+// Вывести все целые числа от A до B включительно; при этом каждое число должно выводиться столько раз,
+// каково его значение (например, число 3 выводится 3 раза).
+// Console.Write("Enter A: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter B: ");
+// int b = int.Parse(Console.ReadLine()!);
+// for (int i = a; i <= b; i++)
+// {
+//     for (int j = 0; j < i; j++)
+//     {
+//         Console.Write(i + " ");
+//     }
+// }
 
-
-
-
-
-
-
-
-
-
-
+// For40. Даны целые числа A и B (A < B). Вывести все целые числа от A до B включительно;
+// при этом число A должно выводиться 1 раз, число A + 1 должно выводиться 2 раза и т. д.
+// Console.Write("Enter A: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter B: ");
+// int b = int.Parse(Console.ReadLine()!);
+// for (int i = a; i <= b; i++)
+// {
+//     for (int j = 0; j <= i - a; j++) 
+//     {
+//         Console.Write(i + " ");
+//     }
+// }
---
+++ AbramyanAnswerBook/While.cs
@@ -0,0 +1,231 @@
+// While 1. Даны положительные числа A и B (A > B).
+// На отрезке длины A размещено максимально возможное количество отрезков длины B (без наложений).
+// Не используя операции умножения и деления, найти длину незанятой части отрезка A.
+// Console.Write("A: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("B: ");
+// int b = int.Parse(Console.ReadLine()!);
+// while (a >= b)
+// {
+//     a -= b;
+// }
+// Console.WriteLine($"Result: {a}");
+
+//While 2. Даны положительные числа A и B (A > B).
+//На отрезке длины A размещено максимально возможное количество отрезков длины B (без наложений).
+//Не используя операции умножения и деления, найти количество отрезков B, размещенных на отрезке A.
+// Console.Write("A: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("B: ");
+// int b = int.Parse(Console.ReadLine()!);
+// int result = 0;
+// while (b <= a)
+// {
+//     a -= b;
+//     result++;
+// }
+// Console.WriteLine(result);
+
+
+//While 3. Даны целые положительные числа N и K.
+//Используя только операции сложения и вычитания, найти частное от деления нацело N на K,
+//а также остаток от этого деления.
+// Console.Write("N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("K: ");
+// int k = int.Parse(Console.ReadLine()!);
+// int quotient = 0;
+// while (k <= n)
+// {
+//     n -= k;
+//     quotient++;
+// }
+// Console.WriteLine($"Quotient: {quotient}");
+// Console.WriteLine($"Reminder: {n}");
+
+// While 4. Дано целое число N (>0). Если оно является степенью числа 3,
+//то вывести TRUE, если не является  вывести FALSE.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// while (n >= 3 && n % 3 == 0)
+// {
+//     n /= 3;
+// }
+// Console.WriteLine(n == 1);
+
+// While 5. Дано целое число N (> 0), являющееся некоторой степенью числа 2:
+// N = 2^K. Найти целое число K — показатель этой степени
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = 0;
+// while (n > 1)
+// {
+//     n /= 2;
+//     k++;
+// }
+// Console.WriteLine($"K: {k}");
+
+// While 7. Дано целое число N (> 0). Найти наименьшее целое положительное число K, квадрат которого превосходит N:
+// K^2 > N. Функцию извлечения квадратного корня не использовать.
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = 1;
+// while (k * k <= n) 
+// {
+//     k++;
+// }
+// Console.WriteLine($"K: {k}");
+
+// While 8. Дано целое число N (> 0). Найти наибольшее целое число K, квадрат которого не превосходит N:
+// K 2 ≤ N. Функцию извлечения квадратного корня не использовать.
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = 1;
+// while (k * k <= n)
+// {
+//     k++;
+// }
+// Console.WriteLine($"K: {--k}");
+
+// While 9. Дано целое число N (> 1). Найти наименьшее целое число K, при котором выполняется неравенство 3K > N.
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = 0, power = 1; 
+// while (power <= n)
+// {
+//     power *= 3;
+//     k++;
+// }
+// Console.WriteLine($"K: {k}");
+
+// While 10. Дано целое число N (> 1). Найти наибольшее целое число K, при котором выполняется неравенство 3K < N.
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = 0, power = 1;
+// while (power * 3 < n)
+// {
+//     power *= 3;
+//     k++;
+// }
+// Console.WriteLine($"K: {k}");
+
+// While 11 . Дано целое число N (> 1). Вывести наименьшее из целых чисел K,
+// для которых сумма 1 + 2 + . . . + K будет больше или равна N, и саму эту сумму.
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = 0, sum = 0;
+// while (sum < n)
+// {
+//     k++;
+//     sum += k;
+// }
+// Console.WriteLine($"K = {k}, Сумма = {sum}");
+
+// While 12. Дано целое число N (> 1). Вывести наибольшее из целых чисел K,
+// для которых сумма 1 + 2 + . . . + K будет меньше или равна N, и саму эту сумму
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = 0, sum = 0;
+// while (sum + k + 1 <= n)
+// {
+//     sum += ++k;
+// }
+// Console.WriteLine($"k = {k}, sum = {sum}");
+
+//While 20. Дано целое число N (> 0). С помощью операций деления нацело и взятия остатка от деления определить,
+//имеется ли в записи числа N цифра «2». Если имеется, то вывести TRUE, если нет — вывести FALSE.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// bool hasTwo = false;
+// while (n > 0)
+// {
+//     if (n % 10 == 2)
+//         hasTwo = true;
+//
+//     n /= 10;
+// }
+// Console.WriteLine(hasTwo);
+
+// While 21. Дано целое число N (> 0). С помощью операций деления нацело и взятия остатка от деления определить,
+// имеются ли в записи числа N нечетные цифры. Если имеются, то вывести TRUE, если нет — вывести FALSE.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// bool hasOdd = false;
+// while (n > 0)
+// {
+//     if ((n % 10) % 2 == 1)
+//         hasOdd = true; 
+//     
+//     n /= 10;
+// }
+// Console.WriteLine(hasOdd);
+
+// While 22. Дано целое число N (> 1). Если оно является простым, то есть не имеет положительных делителей,
+// кроме 1 и самого себя, то вывести TRUE,иначе вывести FALSE.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// bool isPrime = n > 1;
+// int i = 2;
+// while (i * i <= n)
+// {
+//     if (n % i == 0) 
+//         isPrime = false; 
+//     
+//     i++;
+// }
+// Console.WriteLine(isPrime);
+
+// While 24. Дано целое число N (> 1). Последовательность чисел Фибоначчи FK определяется следующим образом:
+// F1 = 1, F2 = 1, FK = FK−2 + FK−1, K = 3, 4, . . . . Проверить, является ли число N числом Фибоначчи.
+// Если является, то вывести TRUE, если нет — вывести FALSE.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int f1 = 1, f2 = 1, fk = 1;
+// while (fk < n) 
+// {
+//     fk = f1 + f2;
+//     f1 = f2;
+//     f2 = fk;
+// }
+// Console.WriteLine(fk == n);
+
+
+// While 25. Дано целое число N (> 1). Найти первое число Фибоначчи, большее N.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int f1 = 1, f2 = 1, fk = 1;
+// while (fk <= n) 
+// {
+//     fk = f1 + f2;
+//     f1 = f2;
+//     f2 = fk;
+// }
+// Console.WriteLine(fk);
+
+// While 26. Дано целое число N (> 1), являющееся числом Фибоначчи: N = FK.
+// Найти целые числа FK−1 и FK+1 — предыдущее и последующее числа Фибоначчи.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int f1 = 1, f2 = 1, fk = 1;
+// while (fk < n) 
+// {
+//     fk = f1 + f2;
+//     f1 = f2;
+//     f2 = fk;
+// }
+// Console.WriteLine($"prev = {f1}, next = {f1 + f2}");
+
+// While 27. Дано целое число N (> 1), являющееся числом Фибоначчи: N = FK
+// Найти целое число K — порядковый номер числа Фибоначчи N.
+// Console.Write("Enter N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int k = 2;
+// int f1 = 1, f2 = 1, fk = 1;
+// while (fk < n) 
+// {
+//     fk = f1 + f2;
+//     f1 = f2;
+//     f2 = fk;
+//     k++;
+// }
+// Console.WriteLine($"k = {k}");
