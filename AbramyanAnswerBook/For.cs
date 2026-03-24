7bf5ce95eb16c473d4c44a007eb0944e8836672d
Avazbek Olimov <avazbekolimov722@gmail.com>
2026-03-24 22:08:59 +0300

    added for14 variant 3

--- AbramyanAnswerBook/For.cs
+++ AbramyanAnswerBook/For.cs
@@ -202,7 +202,7 @@
 // Console.Write("Enter N: ");
 // int n = int.Parse(Console.ReadLine()!);
 // int sum = 0;
-// for (int i = 0, j = 1; i < n; i++, j += 2) 
+// for (int i = 0, j = 1; i < n; i++, j += 2)
 // {
 //     sum += j;
 // }
@@ -217,9 +217,22 @@
 // int result = 0;
 // for (int i = 1; i <= n; i++)
 // {
-//     result += 2 * i - 1; 
+//     result += 2 * i - 1;
 // }
 // Console.WriteLine($"Result = {result}");
+
+// For 14. Дано целое число N (>0). Найти квадрат данного числа, используя для его вычисления следующую формулу:
+// N^2 = 1 + 3 + 5 + ... + (2*N - 1). После добавления к сумме каждого слагаемого выводить текущее
+// значение суммы (в результате будут выведены квадраты всех целых чисел от 1 до N).
+// Variant # 3
+// Console.Write("Enter N: ");
+// int  n = int.Parse(Console.ReadLine()!);
+// int sum = 0;
+// for (int i = 1; i <= 2 * n - 1; i += 2)
+// {
+//     sum += i;
+//     Console.WriteLine(sum);
+// }
 
 // For 15. Дано вещественное число A и целое число N (> 0). Найти A в степени N: A^N = A·A· . . . ·A (Метод для возведения в степень не использовать).
 // Console.Write("Enter A: ");
